a1e2b44d64982e38e0e39c573e45aa47a81efd7a
AlexAviles345 <20233tn198@utez.edu.mx>
2025-10-19 21:22:44 -0600

    [ADD] Entidad de TipoVehiculo
    Se añadió la nueva entidad de TipoVehiculo

---
+++ backendparking/src/main/java/utez/edu/mx/backendparking/modules/tipovehiculo/domain/TipoVehiculo.java
@@ -0,0 +1,44 @@
+package utez.edu.mx.backendparking.modules.tipovehiculo.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tipo_vehiculo")
+public class TipoVehiculo {
+
+    // ATRIBUTOS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+
+    // CONSTRUCTORES
+    public TipoVehiculo() {
+    }
+
+    public TipoVehiculo(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+
+    // GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
