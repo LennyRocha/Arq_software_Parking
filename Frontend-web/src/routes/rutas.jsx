1f516f7eca9fc741f7c0dd38fd9b0759c9256631
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2025-11-09 19:53:52 -0600

    [FIX] imports perdidos con el merge

--- Frontend-web/src/routes/rutas.jsx
+++ Frontend-web/src/routes/rutas.jsx
@@ -7,7 +7,8 @@ import PensionadoRouter from "./PensionadoRouter";
 import Pages from "../modules/cajon/pages/Pages";
 import AdminGestionarEntradasSalidas from "../modules/entradas_salidas/pages/AdminGestionarEntradasSalidas";
 import AdminGestionTarifas from "../modules/tarifas/pages/AdminGestionTarifas";
-
+import GestionTiposPension from "../modules/tipo_pension/pages/GestionTiposPension";
+import PensionesUsuario from "../modules/tipo_pension/pages/PensionesUsuarios";
 
 const Err = () => <h1>404 - Not Found!</h1>;
 
@@ -29,7 +30,7 @@ export default function Rutas() {
           <Route index element={<AdminGestionarEntradasSalidas />} />
           <Route path="reportes" element={<Pages />} />
           <Route path="tipos_de_pension" element={<GestionTiposPension/>} />
-          <Route path="pensiones_de_usuarios" element={<PensionesUsuarios />} />
+          <Route path="pensiones_de_usuarios" element={<PensionesUsuario />} />
           <Route path="tarifas" element={<AdminGestionTarifas />} />
           <Route path="cajones" element={<Pages />} />
           <Route path="gestion_empleados" element={<Pages />} />
