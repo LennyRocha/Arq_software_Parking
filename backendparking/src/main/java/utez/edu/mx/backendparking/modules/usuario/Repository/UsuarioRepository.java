76829339e767c98135364cdbec9c07e1bb1130db
LennyRocha <20233tn094@utez.edu.mx>
2025-11-24 14:01:08 -0600

    Fix

--- backendparking/src/main/java/utez/edu/mx/backendparking/modules/usuario/Repository/UsuarioRepository.java
+++ backendparking/src/main/java/utez/edu/mx/backendparking/modules/usuario/Repository/UsuarioRepository.java
@@ -1,7 +1,7 @@
-package utez.edu.mx.backendparking.modules.usuario.Repository;
+package utez.edu.mx.backendparking.modules.usuario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import utez.edu.mx.backendparking.modules.usuario.Usuario;
+import utez.edu.mx.backendparking.modules.usuario.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
--- backendparking/src/main/java/utez/edu/mx/backendparking/modules/usuario/controller/UsuarioController.java
+++ backendparking/src/main/java/utez/edu/mx/backendparking/modules/usuario/controller/UsuarioController.java
@@ -1,12 +1,14 @@
-package utez.edu.mx.backendparking.modules.usuario.controller;
+package utez.edu.mx.backendparking.modules.usuario.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import utez.edu.mx.backendparking.modules.usuario.UsuarioService;
 import utez.edu.mx.backendparking.modules.usuario.Request.UsuarioRequest;
-import utez.edu.mx.backendparking.modules.usuario.model.Usuario;
 import utez.edu.mx.backendparking.shared.api.ApiResponse;
 
 @RestController
@@ -23,10 +25,5 @@ public class UsuarioController {
         return ResponseEntity
             .status(response.getStatus())
             .body(response);
-    }
-
-    @GetMapping("usuarios/backend/{id}")
-    public Usuario getUserForBackend(@PathVariable("id") Long id) {
-        return usuarioService.getUserForBackend(id);
     }
 }
