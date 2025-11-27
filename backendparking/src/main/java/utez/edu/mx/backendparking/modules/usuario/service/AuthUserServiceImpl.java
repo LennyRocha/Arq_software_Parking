39412c179ee703df988e44e239e0065df800f526
Oscar Uriel Montes Patiño <20233tn073@utez.edu.mx>
2025-11-27 01:08:43 -0600

    Corrijo la respuesta del backend con el Login

--- backendparking/src/main/java/utez/edu/mx/backendparking/modules/usuario/service/AuthUserServiceImpl.java
+++ backendparking/src/main/java/utez/edu/mx/backendparking/modules/usuario/service/AuthUserServiceImpl.java
@@ -3,6 +3,9 @@ package utez.edu.mx.backendparking.modules.usuario.service;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import utez.edu.mx.backendparking.security.JWTUtils;
 import utez.edu.mx.backendparking.shared.api.ApiResponse;
@@ -63,26 +66,33 @@ public class AuthUserServiceImpl {
     }
 
     public ApiResponse<?> Login(String correo, String contra) {
-        try {
-            Usuario usuario = usuarioRepository.findByCorreo(correo);
+   Usuario usuario = usuarioRepository.findByCorreo(correo);
 
-            if (!passwordEncoder.matches(contra, usuario.getContra())) {
-                return ApiResponse.error(HttpStatus.UNAUTHORIZED, "Credenciales incorrectas", null);
-            }
-
-            if (!usuario.isStatus()) {
-                return ApiResponse.error(HttpStatus.BAD_REQUEST, "Tu cuenta esta desactivada", null);
-            }
-
-            String role = usuario.getRol().getName().name();
-            Long id = usuario.getId();
-            String token = jwtUtils.generateAccessToken(correo, role, id);
-            return ApiResponse.success(HttpStatus.OK, token, null);
-
-        } catch (Exception e) {
-            return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "No se pudo iniciar sesion", null);
-        }
+    if (usuario == null || !passwordEncoder.matches(contra, usuario.getContra())) {
+        return ApiResponse.error(HttpStatus.UNAUTHORIZED, "Credenciales incorrectas", null);
     }
+
+    if (!usuario.isStatus()) {
+        return ApiResponse.error(HttpStatus.BAD_REQUEST, "Tu cuenta está desactivada", null);
+    }
+
+    String role = usuario.getRol().getName().name();
+    Long id = usuario.getId();
+
+    String token = jwtUtils.generateAccessToken(correo, role, id);
+
+    
+    Map<String, Object> data = new HashMap<>();
+    data.put("token", token);
+    data.put("expiration", System.currentTimeMillis() + (1000 * 60 * 60)); 
+    data.put("user", Map.of(
+            "id", id,
+            "correo", correo,
+            "role", role
+    ));
+
+    return ApiResponse.success(HttpStatus.OK, "Inicio exitoso", data);
+}
 
 
 }
