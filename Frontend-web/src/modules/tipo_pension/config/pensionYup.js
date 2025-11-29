90b7b2e281bcbae259d84770c1b6894a9a4229eb
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2025-11-29 11:37:54 -0600

    [fix] mensajes tipo de pension igual que el dfr

--- Frontend-web/src/modules/tipo_pension/config/pensionYup.js
+++ Frontend-web/src/modules/tipo_pension/config/pensionYup.js
@@ -3,15 +3,15 @@ import * as yup from "yup";
 const pensionYup = yup.object({
   id: yup.number(),
   nombre: yup.string()
-    .required("El nombre es requerido")
+    .required("¡Campo obligatorio!. Debe completar todos los campos requeridos.")
     .min(3, "¡Nombre inválido!. El nombre de la pensión debe tener entre 3 y 50 caracteres")
     .max(50, "¡Nombre inválido!. El nombre de la pensión debe tener entre 3 y 50 caracteres"),
   duracionDias: yup.number()
-    .required("La duración es requerida")
+    .required("¡Campo obligatorio!. Debe completar todos los campos requeridos.")
     .min(7, "¡Duración inválida!. La duración mínima es de 7 días.")
     .max(365, "¡Duración inválida!. La duración máxima es de 365 días."),
   costo: yup.number()
-    .required("El costo es requerido")
+    .required("¡Campo obligatorio!. Debe completar todos los campos requeridos.")
     .min(1, "¡Costo inválido!. El monto debe ser mayor a 0"),
   estatus: yup.boolean()
     .default(true),
